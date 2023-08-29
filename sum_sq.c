b489e227251730935f0508b2991706a7576f9ebf
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-08-29 14:11:35 -0700

    Update sum_sq.c

--- sum_sq.c
+++ sum_sq.c
@@ -4,14 +4,5 @@
 #include "sum_sq.h"
 
 void sum_sq() {
-	int x, y;
-	double x_sq, y_sq, sum;
-	printf("Enter two integers:\n");
-	scanf("%d%d", &x, &y);
 
-	// sum = x*x + y*y;
-	x_sq = pow(x, 2);
-	y_sq = pow(y, 2);
-	sum = x_sq + y_sq;
-	printf("\nSum of squares = %f\n", sum);
 }
